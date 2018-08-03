1f9b91cc69d006cf3f3fe54ee892e5367fce8aef
PHEBERAICHELJOHN <pheberaichaljohn@gmail.com>
2018-08-03 22:31:09 +0530

    function

---
+++ inside_function.py
@@ -0,0 +1,21 @@
+#!user/bin/env python3
+
+def my_func():
+	 x = 10;
+         print("Value is ",x);
+
+x = 30;
+my_func()
+print("value is ",x);
+
+"""
+
+def my_func():
+	x = 10
+	print("Value inside function:",x)
+
+x = 20
+my_func()
+print("Value outside function:",x)
+
+"""
